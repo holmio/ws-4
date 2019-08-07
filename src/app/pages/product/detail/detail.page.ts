cbfa32eb80db3b1174bba36b552a4a578d23cfd4
Mohamed Saila <holmio666@gmail.com>
2019-08-07 21:45:02 +0200

    added modal slider

--- src/app/pages/product/detail/detail.page.ts
+++ src/app/pages/product/detail/detail.page.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { ProductState, Product, GetProductAction, AddFavoriteAction, RemoveFavoriteAction, DeleteProductAction } from 'src/app/store/product';
 import { Observable } from 'rxjs';
 import { Select, Store } from '@ngxs/store';
+import { ModalController } from '@ionic/angular';
+import { ModalSlidersComponent } from 'src/app/components/modal-sliders/modal-sliders.component';
 
 @Component({
   selector: 'app-detail',
@@ -16,13 +18,14 @@ export class DetailPage implements OnInit {
   @Select(ProductState.getProduct) product$: Observable<Product>;
   @Select(ProductState.getIsUserProduct) isUserProduct$: Observable<boolean>;
   id: string;
-  slidesOpts = {
+  slideOpts = {
     centeredSlides: true,
     preloadImages: false,
     lazy: true,
   };
   constructor(
     private route: ActivatedRoute,
+    private modalController: ModalController,
     private store: Store,
   ) {
     this.id = this.route.snapshot.params.id;
@@ -42,6 +45,13 @@ export class DetailPage implements OnInit {
 
   delete() {
     this.store.dispatch(new DeleteProductAction());
+  }
+
+  private async presentModal() {
+    const modal = await this.modalController.create({
+      component: ModalSlidersComponent,
+    });
+    return await modal.present();
   }
 
 }
