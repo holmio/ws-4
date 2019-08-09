20fd721d3d4b486a92b921da18449048ea610341
Mohamed El Jalil <m.eljalilmami@shoop.de>
2019-08-09 12:38:38 +0200

    removed pipe

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,7 +4,6 @@ import { RouterModule } from '@angular/router';
 import { HomePage } from './home.page';
 import { SharedModule } from 'src/app/shared/shared.modue';
 import { ProductCardModule } from 'src/app/components/product-card/product-card.module';
-import { RemoveCurrentUserPipe } from 'src/app/pipes/remove-current-user.pipe';
 
 @NgModule({
   imports: [
@@ -17,6 +16,6 @@ import { RemoveCurrentUserPipe } from 'src/app/pipes/remove-current-user.pipe';
       }
     ])
   ],
-  declarations: [HomePage, RemoveCurrentUserPipe],
+  declarations: [HomePage],
 })
 export class HomePageModule {}
